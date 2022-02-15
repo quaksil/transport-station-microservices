754c29c71a90e24902c3652d5a7768096667b2a7
Tarik <taryk.haroun@gmail.com>
2022-02-15 22:08:43 +0100

    started eureka deployement and front-end for CRUD

---
+++ micro-passenger-service/src/main/java/com/archlog/micropassenger/MicroPassengerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.archlog.micropassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroPassengerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPassengerServiceApplication.class, args);
+	}
+
+}
